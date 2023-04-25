008164dd018b7261c95fb1e2228fb5a974e9a87a
Mary <meditation708@gmail.com>
2023-04-25 05:52:25 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desciption: 'print putchar followed by new line'
+ * Return always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
