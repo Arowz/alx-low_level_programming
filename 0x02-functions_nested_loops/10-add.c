ab3984a52e2a2daa4e7e0b5de6a7fc2524d1ecb0
Mary <meditation708@gmail.com>
2023-04-27 12:27:26 -0700

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @n1: the first number to be added
+ * @n2: the next number to be added
+ * Return: integer
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
