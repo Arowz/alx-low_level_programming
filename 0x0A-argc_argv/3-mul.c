25e1b7ec2ef2f9c15577c140e9cb9bc1ea5a3dbd
Mary <meditation708@gmail.com>
2023-05-17 10:52:33 -0700

    multiplies only two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,74 @@
-#include <stdlib.h>
 #include "main.h"
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * _atoi -To  convert a string to an integer.
+ * @s: input the string.
+ *
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+
+/**
+ * main - multiplies two numbers
  * @argc: - count argument
  * @argv: - array of strings
  *
- * return: 0 always
+ * Return: 0 always or 1 for error
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
 	int a;
 	int b;
 	int mul;
 
-	if (argc > 1)
+	if (argc < 3 || argc > 3)
 	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		(a * b);
+		printf("Error\n");
+		return (1);
 	}
-        return (0);
+		a = _atoi(argv[1]);
+		b = _atoi(argv[2]);
+		mul = a * b;
+		printf("%d\n", mul);
+		return (0);
 }
